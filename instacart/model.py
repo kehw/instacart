bb6bc2e5751adf77d085ec2ca2d34f888bef611a
Hongwei Ke <kehw1984@gmail.com>
2017-07-09 23:08:54 -0400

    order_number shows up twice

--- instacart/model.py
+++ instacart/model.py
@@ -9,7 +9,7 @@ def build_estimator(model_dir,model_type):
     order_hour_of_day = tf.contrib.layers.real_valued_column("order_hour_of_day")
     days_since_prior_order = tf.contrib.layers.real_valued_column("days_since_prior_order")
 
-    f_columns = [ reorder, user_id, order_number, order_number, order_dow, order_hour_of_day, days_since_prior_order]
+    f_columns = [ reorder, user_id, order_number, order_dow, order_hour_of_day, days_since_prior_order]
 
     if model_type=="linearClassifier":
         m = tf.contrib.learn.LinearClassifier(model_dir=model_dir, feature_columns=f_columns)
